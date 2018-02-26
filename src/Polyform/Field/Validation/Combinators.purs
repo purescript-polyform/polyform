53c032c31d8886ecbdc06ff797ac0a0c7d8d51e6
Tomasz Rybarczyk <paluho@gmail.com>
2018-02-26 17:15:54 +0100

    Start extracting field validation combinators

---
+++ src/Polyform/Field/Validation/Combinators.purs
@@ -0,0 +1,19 @@
+module Polyform.Field.Validation.Combinators where
+
+import Prelude
+
+import Data.Either (note)
+import Data.Int (fromString)
+import Data.Variant (Variant)
+import Polyform.Field.Validation (Validation, liftEither, tag)
+import Type.Prelude (SProxy(..))
+
+_int ∷ SProxy "int"
+_int = SProxy
+
+int
+  ∷ ∀ m e
+  . Monad m
+  ⇒ Validation m (Variant (int ∷ String | e)) String Int
+int = tag _int $ liftEither $ note <*> fromString
+
