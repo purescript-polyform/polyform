b5934aa545e73abf4f0e3dcc1854c9328128315d
Tomasz Rybarczyk <paluho@gmail.com>
2018-03-05 19:27:45 +0100

    Form.Validation.Par: initial stub

---
+++ src/Polyform/Form/Validation/Par.purs
@@ -0,0 +1,17 @@
+module Polyform.Form.Validation.Par where
+
+import Prelude
+
+import Control.Parallel (class Parallel, parallel, sequential)
+import Data.Newtype (class Newtype)
+import Polyform.Form.Validation (Validation(..))
+
+newtype ParValidation m e a b = ParValidation (Validation m e a b)
+derive instance newtypeVaildation ∷ Newtype (ParValidation m e a b) _
+derive instance functorParValidation ∷ (Functor m) ⇒ Functor (ParValidation m e a)
+
+instance applyParValidation ∷ (Monad m, Parallel f m, Semigroup e) ⇒ Apply (ParValidation m e a) where
+  apply (ParValidation (Validation mf)) (ParValidation (Validation ma)) =
+    ParValidation $ Validation \i →
+      sequential $ (\vf va → vf <*> va) <$> parallel (mf i) <*> parallel (ma i)
+
