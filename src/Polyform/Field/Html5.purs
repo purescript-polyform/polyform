dcb46fed876548d3d38d12d577f1b66b212195b3
Tomasz Rybarczyk <paluho@gmail.com>
2018-03-19 18:18:35 +0100

    Field.Html5: Change range inputs representation

--- src/Polyform/Field/Html5.purs
+++ src/Polyform/Field/Html5.purs
@@ -3,42 +3,13 @@ module Polyform.Field.Html5 where
 import Prelude
 
 import Data.Array (singleton)
-import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe, maybe)
 import Data.String (length)
 import Data.Variant (Variant)
 import Polyform.Field (Input)
 import Polyform.Field.Validation.Combinators (check, checkAndTag)
-import Polyform.Validation (V, Validation)
-import Type.Prelude (class IsSymbol, SProxy(SProxy), reflectSymbol)
-
--- | RangeInput can be used to represent
--- | `type="range"` and `type="number"`
--- | of Integer values
-type RangeInputErr e = Variant (min ∷ Int, max ∷ Int | e)
-data RangeInputType = RangeInput | NumberInput
-derive instance genericRangeInputType ∷ Generic RangeInputType _
-
-showRangeInputType ∷ RangeInputType → String
-showRangeInputType RangeInput = "range"
-showRangeInputType NumberInput = "number"
-
-_min = SProxy ∷ SProxy "min"
-_max = SProxy ∷ SProxy "max"
-
-type RangeInputBase err attrs name value =
-  { name ∷ name
-  , min ∷ Maybe Int
-  , max ∷ Maybe Int
-  , step ∷ Int
-  , type ∷ RangeInputType
-  , value ∷ V (Array (RangeInputErr err)) value
-  | attrs
-  }
-
-type RangeInput err attrs name = RangeInputBase err attrs (SProxy name) Int
-type OptRangeInput err attrs name = RangeInputBase err attrs (SProxy name) (Maybe Int)
-
+import Polyform.Validation (Validation)
+import Type.Prelude (class IsSymbol, SProxy(SProxy))
 
 check'
   ∷ ∀ a m
@@ -57,10 +28,32 @@ checkAndTag'
   → Validation m (Array (Variant e)) a a
 checkAndTag' = checkAndTag singleton
 
+
+-- | RangeInput can be used to represent
+-- | `type="range"` and `type="number"`
+-- | of Integer values
+type RangeInputErr err = (min ∷ Int, max ∷ Int | err)
+
+_min = SProxy ∷ SProxy "min"
+_max = SProxy ∷ SProxy "max"
+
+type RangeInputBase (type_ ∷ Symbol) attrs name err value =
+  Input
+    (min ∷ Maybe Int, max ∷ Maybe Int, step ∷ Maybe Int | attrs)
+    name
+    (Array (Variant (RangeInputErr err)))
+    value
+
+type RangeInput attrs name err = RangeInputBase "range" attrs name err Int
+type OptRangeInput attrs name err = RangeInputBase "range" attrs name err (Maybe Int)
+
+type NumberInput attrs name err = RangeInputBase "number" attrs name err Int
+type OptNumberInput attrs name err = RangeInputBase "number" attrs name err (Maybe Int)
+
 rangeInputValidation
   ∷ forall attrs err name m
   . Monad m
-  ⇒ RangeInput err attrs name
+  ⇒ RangeInput attrs name err
   → Validation
       m
       (Array (Variant (min ∷ Int, max ∷ Int | err)))
@@ -106,13 +99,6 @@ type OptTextInput attrs name err = TextInputBase "text" attrs name err (Maybe St
 type UrlInput attrs name err = TextInputBase "url" attrs name err String
 type OptUrlInput attrs name err = TextInputBase "url" attrs name err (Maybe String)
 
-textInputType
-  ∷ ∀ attrs err name type_ value
-  . IsSymbol type_
-  ⇒ TextInputBase type_ attrs name err value
-  → String
-textInputType _ = reflectSymbol (SProxy ∷ SProxy type_)
-
 _maxlength = SProxy ∷ SProxy "maxlength"
 _minlength = SProxy ∷ SProxy "minlength"
 
@@ -120,7 +106,11 @@ textInputValidation
   ∷ ∀ attrs err m
   . Monad m
   ⇒ { maxlength ∷ Maybe Int, minlength ∷ Maybe Int | attrs }
-  → Validation m (Array (Variant (TextInputErr err))) String String
+  → Validation
+      m
+      (Array (Variant (TextInputErr err)))
+      String
+      String
 textInputValidation r =
   maxV >>> minV
  where
