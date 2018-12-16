66728fafee0903009e3bb83fd8f298c081e7a21d
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-16 21:33:31 +0100

    Add runners for Dual.Validator

--- src/Polyform/Dual/Validator.purs
+++ src/Polyform/Dual/Validator.purs
@@ -1,9 +1,19 @@
 module Polyform.Dual.Validator where
 
+import Prelude
+
+import Data.Newtype (unwrap)
+import Data.Validation.Semigroup (V)
 import Polyform.Dual (Dual)
 import Polyform.Validator as Validator
 import Polyform.Validator.Par as Validator.Par
 
 type Validator m e i o = Dual (Validator.Validator m e) i o
+
+runValidator ∷ ∀ e i o m. Dual (Validator.Validator m e) i o → (i → m (V e o))
+runValidator = Validator.runValidator <<< _.parser <<< unwrap <<< unwrap
+
+runSerializer ∷ ∀ e i o m. Dual (Validator.Validator m e) i o → (o → i)
+runSerializer = _.serializer <<< unwrap <<< unwrap
 
 newtype Par m r a b = Dual (Validator.Par.Par m r a b)
