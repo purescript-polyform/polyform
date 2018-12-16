9d591bb644311f711a0b57316439e4f9880c2752
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-16 22:13:23 +0100

    Add runSerializer' to simplify type inference

--- src/Polyform/Dual/Validator.purs
+++ src/Polyform/Dual/Validator.purs
@@ -10,10 +10,14 @@ import Polyform.Validator.Par as Validator.Par
 
 type Validator m e i o = Dual (Validator.Validator m e) i o
 
-runValidator ∷ ∀ e i o m. Dual (Validator.Validator m e) i o → (i → m (V e o))
+runValidator ∷ ∀ e i o m. Monad m ⇒ Dual (Validator.Validator m e) i o → (i → m (V e o))
 runValidator = Validator.runValidator <<< _.parser <<< unwrap <<< unwrap
 
 runSerializer ∷ ∀ e i o m. Dual (Validator.Validator m e) i o → (o → i)
 runSerializer = _.serializer <<< unwrap <<< unwrap
+
+-- | Dirty hack to simplify type inference for polymorphic duals
+runSerializer' ∷ ∀ e i o m. Applicative m ⇒ Dual (Validator.Validator m e) i o → (o → m i)
+runSerializer' = map pure <<< _.serializer <<< unwrap <<< unwrap
 
 newtype Par m r a b = Dual (Validator.Par.Par m r a b)
