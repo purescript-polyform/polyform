6b38993aba6e56c565e98d91f5653bc1f3de9229
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-22 11:58:18 +0100

    Add "runners" for duals

--- src/Polyform/Dual/Reporter.purs
+++ src/Polyform/Dual/Reporter.purs
@@ -4,13 +4,21 @@ import Prelude
 
 import Polyform.Dual (Dual)
 import Polyform.Dual (dual, parser, serializer) as Dual
-import Polyform.Reporter (Reporter, hoistValidator, hoistValidatorWith, hoistValidatorWith') as Reporter
+import Polyform.Reporter (R, Reporter, hoistValidator, hoistValidatorWith, hoistValidatorWith') as Reporter
+import Polyform.Reporter (runReporter) as Reproter
 import Polyform.Reporter.Par as Reporter.Par
 import Polyform.Validator (Validator) as Validator
 
 type Reporter m e i o = Dual (Reporter.Reporter m e) i o
 
 newtype Par m r a b = Dual (Reporter.Par.Par m r a b)
+
+runReporter ∷ ∀ r i m o. Dual (Reporter.Reporter m r) i o → (i → m (Reporter.R r o))
+runReporter = Reproter.runReporter <<< Dual.parser
+
+-- | Dirty hack to simplify type inference for polymorphic duals
+runSerializer ∷ ∀ e i o m. Applicative m ⇒ Dual (Reporter.Reporter m e) i o → (o → m i)
+runSerializer = map pure <<< Dual.serializer
 
 hoistValidatorWith ∷ ∀ e i m o r
   . Functor m
