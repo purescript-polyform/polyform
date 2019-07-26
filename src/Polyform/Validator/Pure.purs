1ed60d7fef519ba2cc9e7374932aaa260735832b
Tomasz Rybarczyk <paluho@gmail.com>
2019-07-26 23:36:23 +0200

    Star + Compose = Validator

--- src/Polyform/Validator/Pure.purs
+++ src/Polyform/Validator/Pure.purs
@@ -3,9 +3,9 @@ module Polyform.Validator.Pure where
 import Prelude
 
 import Data.Identity (Identity(..))
-import Polyform.Validator (Validator(..)) as Validator
+import Polyform.Validator (Validator, hoist) as Validator
 
 type Validator e i o = Validator.Validator Identity e i o
 
 generalize ∷ ∀ e i m o. Applicative m ⇒ Validator e i o → Validator.Validator m e i o
-generalize (Validator.Validator f) = Validator.Validator \i → let Identity o = f i in pure o
+generalize = Validator.hoist (case _ of Identity o → pure o)
