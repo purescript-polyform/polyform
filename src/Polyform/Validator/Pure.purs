6934367a07625a5cbfafe166b42e88389704d90e
Tomasz Rybarczyk <paluho@gmail.com>
2020-10-15 01:39:31 +0200

    Add Validator.Pure.runValidator

--- src/Polyform/Validator/Pure.purs
+++ src/Polyform/Validator/Pure.purs
@@ -3,9 +3,14 @@ module Polyform.Validator.Pure where
 import Prelude
 
 import Data.Identity (Identity(..))
-import Polyform.Validator (Validator, hoist) as Validator
+import Data.Newtype (un)
+import Data.Validation.Semigroup (V)
+import Polyform.Validator (Validator, hoist, runValidator) as Validator
 
 type Validator e i o = Validator.Validator Identity e i o
 
 generalize ∷ ∀ e i m o. Applicative m ⇒ Validator e i o → Validator.Validator m e i o
 generalize = Validator.hoist (case _ of Identity o → pure o)
+
+runValidator ∷ ∀ e i o. Validator e i o → i → V e o
+runValidator = map (un Identity) <<< Validator.runValidator
